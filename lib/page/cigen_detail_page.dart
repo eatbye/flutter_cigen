7c6d7645c82af5b99bcb9e627f827ec047c02db5
ccj <ccjsmile@163.com>
2019-05-07 23:16:46 +0800

    发音按钮

--- lib/page/cigen_detail_page.dart
+++ lib/page/cigen_detail_page.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cigen/dao/word_dao.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 
 class CigenDetailPage extends StatefulWidget {
   var word;
@@ -28,7 +28,7 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: detailWidget(),
+            child: detailContentWidget(),
           ),
           toolbarWidget()
         ],
@@ -36,49 +36,93 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
     );
   }
 
+  Widget detailContentWidget() {
+    return Stack(
+      children: <Widget>[
+        detailWidget(),
+        Positioned(
+          top: 10,
+          right: 0,
+          child: FlatButton(
+            onPressed: () {
+              playVoiceAction();
+            },
+            child: Icon(
+              CupertinoIcons.volume_up,
+              size: 36,
+              color: Colors.black87,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
-  Widget detailWidget(){
-
+  Widget detailWidget() {
     return ListView(
       padding: EdgeInsets.all(12),
       children: <Widget>[
-        Text(widget.word['word'], style: TextStyle(fontSize: 22.0),),
-        SizedBox(height: 10,),
-        Text('词根词缀', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
-        SizedBox(height: 10,),
+        Text(
+          widget.word['word'],
+          style: TextStyle(fontSize: 22.0),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '词根词缀',
+          style: TextStyle(color: Colors.blue, fontSize: 16.0),
+        ),
+        SizedBox(
+          height: 10,
+        ),
         Padding(
-          padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+          padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 0),
           child: Text(widget.word['notes']),
         ),
-        SizedBox(height: 10,),
-        Text('词典释义', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '词典释义',
+          style: TextStyle(color: Colors.blue, fontSize: 16.0),
+        ),
+        SizedBox(
+          height: 10,
+        ),
         Padding(
-          padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+          padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 0),
           child: Text(widget.word['detail']),
         ),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
         sentenceWidget(),
       ],
     );
   }
 
-  Widget sentenceWidget(){
+  Widget sentenceWidget() {
     String sentence = widget.word['sentence'];
-    if(sentence==null){
+    if (sentence == null) {
       sentence = '';
     }
     sentence = sentence.replaceAll('<em>', '');
     sentence = sentence.replaceAll('</em>', '');
     //print(sentence);
-    if (sentence != ''){
+    if (sentence != '') {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('单词例句', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
-          SizedBox(height: 10,),
+          Text(
+            '单词例句',
+            style: TextStyle(color: Colors.blue, fontSize: 16.0),
+          ),
+          SizedBox(
+            height: 10,
+          ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+            padding: const EdgeInsets.fromLTRB(8.0, 0, 8, 0),
             //child: Text(widget.word['sentence']),
             child: Html(
 //            padding: EdgeInsets.all(12.0),
@@ -87,15 +131,18 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
           ),
         ],
       );
-    }else{
+    } else {
       return Container();
     }
   }
 
-  Widget toolbarWidget(){
+  Widget toolbarWidget() {
     var favoriteIcon = Icon(Icons.favorite_border);
-    if(favorite){
-      favoriteIcon = Icon(Icons.favorite, color: Colors.red,);
+    if (favorite) {
+      favoriteIcon = Icon(
+        Icons.favorite,
+        color: Colors.red,
+      );
     }
     return Container(
       padding: EdgeInsets.all(0),
@@ -103,23 +150,29 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          FlatButton(child: Icon(Icons.arrow_back),onPressed: (){
-            preAction();
-          },),
-          FlatButton(child: favoriteIcon, onPressed: (){
-            favorite = !favorite;
-            setState(() {
-
-            });
-          },),
-          FlatButton(child: Icon(Icons.arrow_forward_ios),onPressed: (){
-            nextAction();
-          },),
+          FlatButton(
+            child: Icon(Icons.arrow_back),
+            onPressed: () {
+              preAction();
+            },
+          ),
+          FlatButton(
+            child: favoriteIcon,
+            onPressed: () {
+              favorite = !favorite;
+              setState(() {});
+            },
+          ),
+          FlatButton(
+            child: Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              nextAction();
+            },
+          ),
         ],
       ),
     );
   }
-
 
   void preAction() async {
     var rowid = widget.word['rowid'];
@@ -127,22 +180,18 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
 
     var word = await WordDao.preWord(rowid, cid);
 
-    if(word == null){
+    if (word == null) {
       //print('已经到本类别第一个单词');
       Fluttertoast.showToast(
-          msg: "已经到本类别第一个单词",
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.blue,
-          textColor: Colors.white,
+        msg: "已经到本类别第一个单词",
+        gravity: ToastGravity.CENTER,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
       );
-
-    }else{
+    } else {
       widget.word = word;
-      setState(() {
-
-      });
+      setState(() {});
     }
-
   }
 
   void nextAction() async {
@@ -151,7 +200,7 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
 
     var word = await WordDao.nextWord(rowid, cid);
 
-    if(word == null){
+    if (word == null) {
       //print('已经到本类别最后一个单词');
       Fluttertoast.showToast(
         msg: "已经到本类别最后一个单词",
@@ -159,12 +208,13 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
         backgroundColor: Colors.blue,
         textColor: Colors.white,
       );
-    }else{
+    } else {
       widget.word = word;
-      setState(() {
-
-      });
+      setState(() {});
     }
   }
 
+  void playVoiceAction(){
+
+  }
 }
