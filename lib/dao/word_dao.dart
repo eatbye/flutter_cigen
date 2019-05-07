48baf93a8813d9f38cf9890042debac607c84ad1
ccj <ccjsmile@163.com>
2019-05-07 23:02:35 +0800

    详细页面上下翻页功能

--- lib/dao/word_dao.dart
+++ lib/dao/word_dao.dart
@@ -5,9 +5,35 @@ class WordDao{
 
   static Future<List<Map>> listWord(itemId) async {
     var db = await DatabaseHelper().db;
-    String sql = 'SELECT * FROM word where cid='+itemId.toString();
+    String sql = 'SELECT rowid, * FROM word where cid='+itemId.toString();
     List<Map> list = await db.rawQuery(sql);
     return list;
+  }
+
+  static preWord(rowid, cid) async {
+    var db = await DatabaseHelper().db;
+    String sql = 'select rowid,* from word where cid=? and rowId<? order by rowid desc';
+    List<Map> list = await db.rawQuery(sql, [cid, rowid]);
+    //print(sql);
+    //print(list.length);
+    if (list.length>0){
+      return list[0];
+    }else{
+      return null;
+    }
+  }
+
+  static nextWord(rowid, cid) async {
+    var db = await DatabaseHelper().db;
+    String sql = 'select rowid,* from word where cid=? and rowId>? order by rowid asc';
+    List<Map> list = await db.rawQuery(sql, [cid, rowid]);
+    //print(sql);
+    //print(list.length);
+    if (list.length>0){
+      return list[0];
+    }else{
+      return null;
+    }
   }
 
 }
--- lib/page/cigen_detail_page.dart
+++ lib/page/cigen_detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cigen/dao/word_dao.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class CigenDetailPage extends StatefulWidget {
   var word;
@@ -24,12 +28,68 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: Container(),
+            child: detailWidget(),
           ),
           toolbarWidget()
         ],
       ),
     );
+  }
+
+
+  Widget detailWidget(){
+
+    return ListView(
+      padding: EdgeInsets.all(12),
+      children: <Widget>[
+        Text(widget.word['word'], style: TextStyle(fontSize: 22.0),),
+        SizedBox(height: 10,),
+        Text('词根词缀', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
+        SizedBox(height: 10,),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+          child: Text(widget.word['notes']),
+        ),
+        SizedBox(height: 10,),
+        Text('词典释义', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
+        SizedBox(height: 10,),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+          child: Text(widget.word['detail']),
+        ),
+        SizedBox(height: 10,),
+        sentenceWidget(),
+      ],
+    );
+  }
+
+  Widget sentenceWidget(){
+    String sentence = widget.word['sentence'];
+    if(sentence==null){
+      sentence = '';
+    }
+    sentence = sentence.replaceAll('<em>', '');
+    sentence = sentence.replaceAll('</em>', '');
+    //print(sentence);
+    if (sentence != ''){
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('单词例句', style: TextStyle(color: Colors.blue, fontSize: 16.0),),
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8.0,0,8,0),
+            //child: Text(widget.word['sentence']),
+            child: Html(
+//            padding: EdgeInsets.all(12.0),
+              data: sentence,
+            ),
+          ),
+        ],
+      );
+    }else{
+      return Container();
+    }
   }
 
   Widget toolbarWidget(){
@@ -43,16 +103,68 @@ class _CigenDetailPageState extends State<CigenDetailPage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          FlatButton(child: Icon(Icons.arrow_back),onPressed: (){},),
+          FlatButton(child: Icon(Icons.arrow_back),onPressed: (){
+            preAction();
+          },),
           FlatButton(child: favoriteIcon, onPressed: (){
             favorite = !favorite;
             setState(() {
 
             });
           },),
-          FlatButton(child: Icon(Icons.arrow_forward_ios),onPressed: (){},),
+          FlatButton(child: Icon(Icons.arrow_forward_ios),onPressed: (){
+            nextAction();
+          },),
         ],
       ),
     );
   }
+
+
+  void preAction() async {
+    var rowid = widget.word['rowid'];
+    var cid = widget.word['cid'];
+
+    var word = await WordDao.preWord(rowid, cid);
+
+    if(word == null){
+      //print('已经到本类别第一个单词');
+      Fluttertoast.showToast(
+          msg: "已经到本类别第一个单词",
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.blue,
+          textColor: Colors.white,
+      );
+
+    }else{
+      widget.word = word;
+      setState(() {
+
+      });
+    }
+
+  }
+
+  void nextAction() async {
+    var rowid = widget.word['rowid'];
+    var cid = widget.word['cid'];
+
+    var word = await WordDao.nextWord(rowid, cid);
+
+    if(word == null){
+      //print('已经到本类别最后一个单词');
+      Fluttertoast.showToast(
+        msg: "已经到本类别最后一个单词",
+        gravity: ToastGravity.CENTER,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white,
+      );
+    }else{
+      widget.word = word;
+      setState(() {
+
+      });
+    }
+  }
+
 }
