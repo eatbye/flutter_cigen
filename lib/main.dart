7d9afe3e4552334abfb00f05f3fc90fa34312fb4
ccj <ccjsmile@163.com>
2019-05-08 16:58:43 +0800

    增加bugly

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cigen/page/bottom_bar_page.dart';
-import 'package:flutter_cigen/page/favorite_list_page.dart';
 import 'package:flutter_cigen/util/counter.dart';
 import 'package:provide/provide.dart';
+import 'package:flutter_bugly/flutter_bugly.dart';
 
 //void main() => runApp(MyApp());
 
 //void main() => runApp(ListViewApp());
 //void main() => runApp(NotebookListPage());
 
-void main(){
+void main() => FlutterBugly.postCatchedException(() {
   //main函数里面引用provide
   var counter = Counter();
   var providers = Providers();
   providers..provide(Provider<Counter>.value(counter));
   runApp(ProviderNode(child: MyApp(),providers: providers,));
+});
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,6 +38,25 @@ class MyApp extends StatelessWidget {
       ),
       home: BottomBarPage(),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    initBugly();
+  }
+
+  //bugly加载
+  void initBugly(){
+    FlutterBugly.init(
+//      androidAppId: "b07803fb45",
+      iOSAppId: "301ca8382b",
+      autoCheckUpgrade: false,
+
+    ).then((_result) {
+      print(_result.message);
+    });
   }
 }
 
